910ae93e12a345f0316356e1e5d84305cd116c43
jprm610 <jprm610@gmail.com>
2025-01-27 09:59:56 -0500

    0.0.3

--- scripts/01_clean_homicidios.R
+++ scripts/01_clean_homicidios.R
@@ -2,6 +2,7 @@ data <- read.csv("data/original/homicidios.csv", na.strings = "Sin dato", string
 
 dim(data) #=> 19647, 36
 
+# 1 ------------------------------------------------------------------------
 # Hallamos las columnas que tengan un solo valor 
 # y las guardamos junto al valor
 colnames(data) <- toupper(colnames(data))
@@ -14,6 +15,21 @@ for (col in names(data)) {
   }
 }
 # Eliminamos dichas columnas
-data <- data[, !(names(data) %in% columnas_valor_unico)]
+data <- data[, !(names(data) %in% names(columnas_valor_unico))]
+
+# 2 ---------------------------------------------------------------------
+# Eliminar columnas cuya porporción de datos faltantes sea >= 40%
+nas_resumen <- list()
+N <- nrow(data)
+for (col in names(data)) {
+  cantidad_na <- sum(is.na(data[[col]]))
+  proporcion_na <- round((cantidad_na / N) * 100, 2)
+  nas_resumen[[col]] <- list(
+    cantidad = cantidad_na,
+    proporcion = proporcion_na
+  )
+}
+columnas_a_eliminar <- names(nas_resumen)[sapply(nas_resumen, function(x) x[["proporcion"]] >= 40)]
+data <- data[, !(names(data) %in% columnas_a_eliminar), drop = FALSE]
 
 str(data)
