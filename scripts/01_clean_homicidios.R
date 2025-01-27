e94768b3410e42d8b4880c86041e551dba99b2a3
jprm610 <jprm610@gmail.com>
2025-01-26 21:15:14 -0500

    0.0.2

--- scripts/01_clean_homicidios.R
+++ scripts/01_clean_homicidios.R
@@ -1,3 +1,19 @@
-data <- read.csv("../data/original/homicidios.csv", stringsAsFactors = FALSE)
+data <- read.csv("data/original/homicidios.csv", na.strings = "Sin dato", stringsAsFactors = FALSE)
 
-head(data)
+dim(data) #=> 19647, 36
+
+# Hallamos las columnas que tengan un solo valor 
+# y las guardamos junto al valor
+colnames(data) <- toupper(colnames(data))
+columnas_valor_unico <- list()
+for (col in names(data)) {
+  valores_unicos <- unique(data[[col]])
+  
+  if (length(valores_unicos) == 1) {
+    columnas_valor_unico[[col]] <- valores_unicos
+  }
+}
+# Eliminamos dichas columnas
+data <- data[, !(names(data) %in% columnas_valor_unico)]
+
+str(data)
